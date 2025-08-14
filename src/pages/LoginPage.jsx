3609f3748d318cbcc20eb6306589183e72474700
Subin Park <psubin0512@gmail.com>
2025-08-14 20:07:16 +0900

    Delete src/pages directory

--- src/pages/LoginPage.jsx
+++
@@ -1,7 +0,0 @@
-
-// src/pages/BenefitDetailPage.jsx
-const LoginPage = () => {
-    return <div>LoginPage (준비 중)</div>;
-};
-
-export default LoginPage;
--- src/pages/NotificationPage.jsx
+++
@@ -1,7 +0,0 @@
-// NotificationPage.jsx
-
-const NotificationPage = () => {
-    return <div>알림 페이지</div>;
-};
-
-export default NotificationPage;
